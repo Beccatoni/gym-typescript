dcfaa5b43b8992584559ae0093af9109bb8c5f3d
Beccatoni <rbccmtni1@gmail.com>
2025-01-22 14:41:27 +0200

    chore: add animations to the headings and actions sections

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -5,6 +5,7 @@ const useMediaQuery = (query: string) => {
 
     useEffect(()=> {
         const media = window.matchMedia(query);
+        console.log(media);
         if(media.matches !== matches){
             setMatches(media.matches);
         }
